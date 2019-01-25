9a3c615b7c79fb363e480d898a1fd497e569e1f5
Brandon Williams <brandonwilliams@email.arizona.edu>
2019-01-25 16:01:49 -0700

    finished Episode 9: Go Go Gadget Tailwind

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,9 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <form action="/projects/create" method="POST">
+    <form action="/projects" method="POST">
         @csrf
 
-        <h1>Create a Project</h1>
+        <h1 class="mb-6">Create a Project</h1>
+
+        <div class="mb-4">
+            <label for="title_input">Title</label><br/>
+            <input type="text" id="title_input" name="title">
+        </div>
+
+        <div class="mb-4">
+            <label for="description_textarea">Description</label><br/>
+            <textarea id="description_textarea" name="description"></textarea>
+        </div>
+
+        <div class="flex">
+            <button type="submit" class="mr-3">Create</button>
+            <a href="/projects">Cancel</a>
+        </div>
     </form>
 @endsection
